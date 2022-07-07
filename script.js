540c495ef778cf6942c1320e5cdb504231f3f1f3
Joanluiz <joanluiz69@gmail.com>
2022-07-06 21:22:44 -0300

    atividade

---
+++ script.js
@@ -0,0 +1,57 @@
+/*
+// Crie um objeto que receba ao menos três propriedades sobre você.
+
+let pessoa = {
+    nome: "Joan",
+    idade: 24,
+    cidade: "Natal-RN"
+}
+// Adicione uma nova propriedade sem alterar seu objeto inicial.
+pessoa.ano = 2022
+// Remova uma propriedade desse objeto.
+delete pessoa.idade;
+
+console.log(pessoa)
+*/
+
+
+
+// Crie um array  chamado "cadastro" contendo ao menos 5 objetos. 
+//Cada objeto deve receber as seguintes propriedades: nome,  idade,  telefone, amigos. 
+//Na propriedade amigos, adicione ao menos 4 itens.
+/*
+let cadastro = [{
+  nome:"Joan",
+  idade: 24, 
+  telefone:20327312,
+  amigos:["João", "Felizberto", "Claúdia", "Ana"],
+  },
+  
+  {
+  nome:"Luiza",
+  idade: 26, 
+  telefone: 23123123,
+  amigos:["Paulo André", "Antônio Cartlos", "Paulo", "PH"],
+  },
+  
+  {
+  nome:"Ana",
+  idade: 12, 
+  telefone:23123312 ,
+  amigos:["João Caetano", "João", "Luiz Otávio", "Joana"],
+  },
+  {
+  nome:"Paulo",
+  idade: 57, 
+  telefone:87823882 ,
+  amigos:["Antônio", "Anderson", "Claúdio", "Clara", "Suzanna"],
+  },
+  {
+  nome:"Cesar",
+  idade: 17, 
+  telefone: 42141422,
+  amigos:["Henrique", "Stefano", "Gustavo", "Lozcauzio"],
+  }]
+// Mostre no console o nome de um amigo de cada lista.
+  console.log(cadastro[4].amigos)
+  */
